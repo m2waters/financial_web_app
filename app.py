dd44a1cb9b3b03e3d781e1606cb2be3e7758cb62
m2waters <mwaters2110@gmail.com>
2024-10-14 15:50:16 +0100

    data pulled from database and send to the front end

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template
-
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connections():
+    conn = sqlite3.connect('database.db')
+    return conn
+
+def convert_tuple_to_list(tup):
+    array = []
+    for i in tup:
+        new_list = list(i)
+        array.append(new_list)
+    return array
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    conn = get_db_connections()
+    data = conn.execute('SELECT * FROM financials').fetchall()
+    data = convert_tuple_to_list(data)
+    return render_template('index.html',
+                           data=data)
