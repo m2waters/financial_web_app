076a6556a65bff1c203e3a1f4ef6d8faf9f42171
m2waters <mwaters2110@gmail.com>
2024-10-14 15:12:23 +0100

    Now intialised the correct table

--- schema.sql
+++ schema.sql
@@ -2,7 +2,13 @@ DROP TABLE IF EXISTS financials;
 
 CREATE TABLE financials (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
-);
+    created TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    company TEXT NOT NULL,
+    volume INTEGER NOT NULL,
+    volume_weighted FLOAT NOT NULL,
+    opening_value FLOAT NOT NULL,
+    closing_value FLOAT NOT NULL,
+    high FLOAT NOT NULL,
+    low FLOAT NOT NULL,
+    trades INTEGER NOT NULL
+    );
