09c3d42b617ebc7f47e28f693806799b019bb1bd
m2waters <mwaters2110@gmail.com>
2024-12-16 10:22:37 +0000

    including selenium as autmoated web surfer

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+link = "https://www.reddit.com/r/investing/comments/1hevl5p/what_did_you_learn_from_the_2008_recession_and/"
 
-r = requests.get("https://www.reddit.com/r/investing/comments/1hevl5p/what_did_you_learn_from_the_2008_recession_and/")
+r = requests.get(link)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 s = soup.find('div', class_='entry_content')
@@ -15,6 +16,9 @@ content_href = soup.find_all('shreddit-post')
 
 print(content)
 
-# driver = webdriver.Firefox()
+driver = webdriver.Firefox()
+driver.get(link)
 
-# driver.get("https://google.co.il / search?q = football")
+driver.implicitly_wait(100)
+
+driver.get("https://google.co.uk/search?q=football")
