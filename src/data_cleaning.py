853f9a9f81b004f76c0554ab6e99102c3f6bd7f1
m2waters <mwaters2110@gmail.com>
2024-12-16 10:23:24 +0000

    added to the src folder to keep things tidy

---
+++ src/data_cleaning.py
@@ -0,0 +1,97 @@
+import requests
+import pandas as pd
+import json
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
+import io
+import base64
+import os
+from dotenv import load_dotenv
+
+
+def pull_data(ticker='AAPL'):
+
+    def convert_unix_to_datetime(unix):
+        return datetime.fromtimestamp(unix/1000)
+
+    def increase_in_value(value):
+        if value > 0:
+            return 1
+        elif value <= 0:
+            return 0
+        
+    def fig_to_base64(fig):
+        img = io.BytesIO()
+        fig.savefig(img,
+                    format='png',
+                    bbox_inches='tight')
+        img.seek(0)
+        return base64.b64encode(img.getvalue())
+    
+    load_dotenv()
+    API_KEY = os.getenv('API_KEY')
+
+    r = requests.get('https://api.polygon.io/v2/aggs/ticker/NVDA/range/15/minute/2023-02-27/2024-02-27?adjusted=true&sort=asc&limit=50000&apiKey=' + API_KEY)
+
+    j = json.loads(r.text)
+
+
+    results = pd.DataFrame(j['results'])
+    results.rename(columns={'v': 'volume', 'vw': 'volume_weighted', 'o': 'opening_value', 'c': 'closing_value', 'h': 'high', 'l': 'low', 't': 'datetime', 'n': 'trades'}, inplace=True)
+
+    results['datetime'] = results['datetime'].apply(convert_unix_to_datetime)
+    results['open_close_difference'] = results['closing_value'] - results['opening_value']
+    results['increase_in_value'] = results['open_close_difference'].apply(increase_in_value)
+
+    print(results.head())
+
+
+    fig, ax = plt.subplots()
+
+    ax.plot(results['datetime'], (results['high']))
+    ax.plot(results['datetime'], (results['low']))
+
+    fig.set_figwidth(18)
+    # ax.plot(results['datetime'], (results['volume_weighted']))
+
+    encoded = fig_to_base64(fig)
+    link = "data:image/png;base64, " + encoded.decode('utf-8')
+
+
+    connection = sqlite3.connect('database.db')
+
+
+    with open('schema.sql') as f:
+        connection.executescript(f.read())
+
+    cur = connection.cursor()
+
+    for i in range(len(results)):
+        
+        cur.execute("INSERT INTO financials (created, company, volume, volume_weighted, opening_value, closing_value, high, low, trades) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (str(results['datetime'][i]),
+                    "Apple",
+                    results['volume'][i],
+                    results['volume_weighted'][i],
+                    results['opening_value'][i],
+                    results['closing_value'][i],
+                    results['high'][i],
+                    results['low'][i],
+                    int(results['trades'][i]),
+                    )                
+                )
+
+    cur.execute("INSERT INTO figures (title, link) VALUES (?, ?)",
+                ("TestPlot",
+                link
+                )
+            )
+
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    pull_data()
