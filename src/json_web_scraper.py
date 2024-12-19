c3286ad108f57cb2a0a31ef4b84ea180a951df1c
m2waters <mwaters2110@gmail.com>
2024-12-19 10:58:14 +0000

    json web scraper now stores the reddit posts in a database

--- src/json_web_scraper.py
+++ src/json_web_scraper.py
@@ -5,9 +5,83 @@ import csv, json, time
 import logging, os
 from dataclasses import dataclass, field, fields, asdict
 from concurrent.futures import ThreadPoolExecutor
+import sqlite3
 
 
-def get_post(url, retries=3):
+def open_db_connection(database='database.db'):
+    connection = sqlite3.connect(database)
+    cur = connection.cursor()
+    return (cur, connection)
+
+@dataclass
+class TempDataStore:
+
+    post_title: str = ""
+    author: str = ""
+    permalink: str = ""
+    upvote_ratio: float = 0.0
+
+    def __post_init__(self):
+        self.validate_string()
+
+    def validate_string(self):
+
+        for field in fields(self):
+
+            if isinstance(getattr(self, field.name), str):
+
+                if getattr(self, field.name) == '':
+                    setattr(self, field.name, "NULL")
+                    continue
+                
+                field_value = getattr(self, field.name)
+                setattr(self, field.name, field_value.strip())
+
+
+class StoreData:
+
+    def __init__(self, storage_queue_limit=100):
+        self.permalinks_seen = []
+        self.storage_queue = []
+        self.storage_queue_limit = storage_queue_limit
+        self.db_connection_open = False
+
+
+    def save_to_db(self):
+        
+        data_to_save = []
+        data_to_save.extend(self.storage_queue)
+        self.storage_queue.clear()
+        if not data_to_save:
+            return
+        for data_point in data_to_save:
+            print(data_point.post_title)
+            cursor, connection = open_db_connection()
+            self.db_connection_open = True
+            cursor.execute(
+                "INSERT INTO reddit_posts (post_title, author, permalink, upvote_ratio) VALUES (?, ?, ?, ?)",
+                (data_point.post_title,
+                data_point.author,
+                data_point.permalink,
+                data_point.upvote_ratio
+                )
+            )
+            connection.commit()
+            connection.close()
+            self.db_connection_open = False
+    def is_duplicate(self, input_data):
+        if input_data.permalink in self.permalinks_seen:
+            return True
+        self.permalinks_seen.append(input_data.permalink)
+        return False
+    
+    def add_data(self, scraped_post):
+        if self.is_duplicate(scraped_post) == False:
+            self.storage_queue.append(scraped_post)
+            if len(self.storage_queue) >= self.storage_queue_limit and self.db_connection_open == False:
+                self.save_to_db()
+
+def get_post(url, retries=3, data_pipeline=None):
     tries = 0
     success = False
 
@@ -28,19 +102,19 @@ def get_post(url, retries=3):
                 children=resp["data"]["children"]
                 for child in children:
                     data = child["data"]
-                    name = data["title"]
-                    author = data["author_fullname"]
-                    permalink = data["permalink"]
-                    upvote_ratio = data["upvote_ratio"]
 
-                    print("Name : ", name)
-                    print("Author : ", author)
-                    print("Permalink : ", permalink)
-                    print("Upvotes : ", upvote_ratio)
+                    post = TempDataStore(
+                        post_title = data["title"],
+                        author = data["author_fullname"],
+                        permalink = data["permalink"],
+                        upvote_ratio = data["upvote_ratio"]
+                    )
+      
+                    data_pipeline.add_data(post)
 
-        except Exception as e:
-            tries += 1
-            print("error in pulling data")
+        # except Exception as e:
+        #     tries += 1
+        #     print("error in pulling data")
     
         finally:
             driver.quit()
@@ -49,4 +123,10 @@ def get_post(url, retries=3):
         json.dump(resp, file)
 
 
-get_post(url="https://www.reddit.com/r/investing.json")
+
+
+
+feed_pipeline = StoreData(storage_queue_limit=100)
+get_post(url="https://www.reddit.com/r/investing.json?limit=100", data_pipeline=feed_pipeline)
+feed_pipeline.save_to_db()
+
